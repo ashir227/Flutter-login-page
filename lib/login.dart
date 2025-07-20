c0ec91daf198f72b1de475659c3b408bbcbccd2a
Ashir Ahmed <ashirahmed810@gmail.com>
2025-07-20 21:19:54 +0500

    add

--- lib/login.dart
+++ lib/login.dart
@@ -8,20 +8,96 @@ class Login extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        backgroundColor: const Color.fromARGB(255, 177, 161, 19),
-        title: Text("Foodie App"),
-        titleTextStyle: TextStyle(
-          fontSize: 35,
-          fontWeight: FontWeight.w500,
-          color: Colors.white,
-          fontStyle: FontStyle.italic,
-          letterSpacing: 1,
-          shadows: [
-            Shadow(blurRadius: 3, color: Colors.black45, offset: Offset(3, 2)),
-          ],
+        backgroundColor: const Color.fromARGB(255, 240, 215, 91),
+        title: RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: ("Foodie"),
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontStyle: FontStyle.italic,
+                  letterSpacing: 1,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 3,
+                      color: Colors.black45,
+                      offset: Offset(3, 2),
+                    ),
+                  ],
+                ),
+              ),
+              TextSpan(
+                text: (" HUB"),
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  letterSpacing: 1,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 3,
+                      color: Colors.black45,
+                      offset: Offset(3, 2),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      body: Image.asset('assets/images/car1.jpeg'),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 60),
+            padding: EdgeInsets.only(right: 110),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Welcome back\n",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Please enter your details",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      height: 2.3,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+
+              child: TextField(
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Enter your Email Address',
+                  border: OutlineInputBorder(),
+                  // labelText: 'Email',
+                  icon: Icon(Icons.email, color: Colors.teal),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
